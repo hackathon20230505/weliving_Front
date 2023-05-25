30618a92b3a2538323e989720e7f5316ed99219a
rit245 <urit245@gmail.com>
2023-05-26 00:35:53 +0900

    [#31] Feat: add PasswordFind.tsx

---
+++ src/pages/PasswordFind.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+type PasswordFindProps = {};
+
+const PasswordFind: FunctionComponent<PasswordFindProps> = () => {
+  return <div>비밀번호 찾기</div>;
+};
+
+export default PasswordFind;
