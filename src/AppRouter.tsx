499c5084bec9e900f4569539c8d58121563db8e2
LAPTOP-GR2A5CGU\이소윤 <99dlthdbs@gmail.com>
2023-05-25 17:01:11 +0900

    [#20] Feat ✨ Show Intro or Home page depoends on has/donest has access token
    acces token 여부에 따라 Intro 페이지를 보여줄지, Home 페이지를 보여줄 지 결정합니다.

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,22 +5,28 @@ import Intro from "./pages/Intro";
 import LogIn from "./pages/LogIn";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
+import axios from "axios";
+import styled from "styled-components";
 type AppRouterProps = {};
 
 const AppRouter: FunctionComponent<AppRouterProps> = () => {
+  const token = axios.defaults.headers.common["Authorization"];
+
   return (
-    <div>
+    <AppRouterContainer>
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/intro" element={<Intro />} />
+          {/* access token이 있을 시 Home 화면, 없을 시 Intro 화면 표시 */}
+          <Route path="/" element={token ? <Home /> : <Intro />} />
           <Route path="/login" element={<LogIn />} />
         </Routes>
       </main>
       <Footer />
-    </div>
+    </AppRouterContainer>
   );
 };
 
 export default AppRouter;
+
+const AppRouterContainer = styled.div``;
