e3eb6767a8f9631740f0782f0d727ebbc2c9c15d
LAPTOP-GR2A5CGU\이소윤 <99dlthdbs@gmail.com>
2023-05-31 01:24:43 +0900

    [#34] Feat: Implement LogIn page
    로그인 페이지 컴포넌트를 구현합니다.

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,10 +1,21 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent } from "react";
+import styled from "styled-components";
+import LogInHeader from "../components/LogIn/LogInHeader";
+import LogInBody from "../components/LogIn/LogInBody";
 type LogInProps = {};
 
 const LogIn: FunctionComponent<LogInProps> = () => {
   return (
-   <div>LogIn</div>
+    <LogInContainer>
+      <LogInHeader />
+      <LogInBody />
+    </LogInContainer>
   );
 };
 
 export default LogIn;
+
+const LogInContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
