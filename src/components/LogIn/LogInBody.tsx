6d35f644dc44e95c10f07a8516864c07baee08f8
LAPTOP-GR2A5CGU\이소윤 <99dlthdbs@gmail.com>
2023-06-03 15:05:21 +0900

    [#40] Feat: Add login validation
    로그인 시 이메일 확인, 비밀번호 유효성 검사 기능을 추가하였습니다.

--- src/components/LogIn/LogInBody.tsx
+++ src/components/LogIn/LogInBody.tsx
@@ -1,9 +1,60 @@
-import { FunctionComponent } from "react";
+import { ChangeEvent, FunctionComponent, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 type LogInBodyProps = {};
 
 const LogInBody: FunctionComponent<LogInBodyProps> = () => {
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userPassword, setUserPassword] = useState<string>("");
+
+  const [isValidUserEmail, setIsValidUserEmail] = useState<boolean>(false);
+  const [isValidUserPassword, setIsValidUserPassword] =
+    useState<boolean>(false);
+
+  /** user email input 값 설정 */
+  const onChangeUserEmailHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const currValue = event.target.value;
+    console.log(currValue);
+
+    if (currValue === null) return;
+
+    setUserEmail(currValue);
+
+    if (isValidUserEmailFunc(currValue) === true) {
+      setIsValidUserEmail(true);
+    } else {
+      setIsValidUserEmail(false);
+    }
+  };
+
+  /** user password input 값 설정 */
+  const onChangeUserPasswordHandler = (
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    const currValue = event.target.value;
+    if (currValue === null) return;
+    setUserPassword(currValue);
+
+    if (isValidUserPasswordFunc(currValue) === true) {
+      setIsValidUserPassword(true);
+    } else {
+      setIsValidUserPassword(false);
+    }
+  };
+
+  /** user email 데이터 유효성 검사 */
+  const isValidUserEmailFunc = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  /** user password 데이터 유효성 검사 */
+  const isValidUserPasswordFunc = (password: string) => {
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
   return (
     <LogInBodyContainer>
       <LogInInputGroupContainer>
@@ -12,7 +63,13 @@ const LogInBody: FunctionComponent<LogInBodyProps> = () => {
           id="userEmail"
           type="text"
           placeholder="예) pmr7348.navaer.com"
+          isValidUserEmail={isValidUserEmail}
+          value={userEmail}
+          onChange={onChangeUserEmailHandler}
         />
+        {isValidUserEmail === false && (
+          <NotValidText>이메일 주소를 확인하세요.</NotValidText>
+        )}
       </LogInInputGroupContainer>
       <LogInInputGroupContainer>
         <LogInInputLabel htmlFor="userPassword">비밀번호</LogInInputLabel>
@@ -20,7 +77,15 @@ const LogInBody: FunctionComponent<LogInBodyProps> = () => {
           id="userPassword"
           type="password"
           placeholder="영문, 숫자, 특수문자 조합 8자리 이상"
+          isValidUserPassword={isValidUserPassword}
+          value={userPassword}
+          onChange={onChangeUserPasswordHandler}
         />
+        {isValidUserPassword === false && (
+          <NotValidText>
+            계정이 존재하지 않거나 비밀번호가 옳바르지 않습니다.
+          </NotValidText>
+        )}
       </LogInInputGroupContainer>
       <LogInButton>로그인</LogInButton>
       <FindPWContainer>
@@ -49,9 +114,21 @@ const LogInInputLabel = styled.label`
   font-weight: 700;
 `;
 
-const LogInInput = styled.input`
+interface ILogInInputTypes {
+  isValidUserEmail?: boolean;
+  isValidUserPassword?: boolean;
+}
+
+const LogInInput = styled.input<ILogInInputTypes>`
   padding: 13px 12px 14px 12px;
-  border-bottom: 1px solid var(--strong-purple-800);
+
+  border-bottom: ${({ isValidUserEmail, isValidUserPassword }) =>
+    isValidUserEmail
+      ? "1px solid var(--strong-purple-800)"
+      : isValidUserPassword
+      ? "1px solid var(--strong-purple-800)"
+      : "1px solid #F31919"};
+  border-radius: 0px;
 
   ::placeholder {
     color: var(--gray-purple);
@@ -85,4 +162,12 @@ const FindPWDescription = styled.span`
   font-size: 14px;
   font-weight: 400;
   color: #999999;
+`;
+
+const NotValidText = styled.p`
+  margin-top: 6px;
+
+  font-weight: 400;
+  font-size: 12px;
+  color: #f31919;
 `;
