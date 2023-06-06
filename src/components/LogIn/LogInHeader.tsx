479db06ffb463f29399911c8a231ef381b23de56
LAPTOP-GR2A5CGU\이소윤 <99dlthdbs@gmail.com>
2023-06-06 14:12:11 +0900

    Feat: Link to Home page when click go back button
    뒤로가기 버튼 클릭 시 Home 페이지로 이동합니다.

--- src/components/LogIn/LogInHeader.tsx
+++ src/components/LogIn/LogInHeader.tsx
@@ -1,11 +1,20 @@
 import { FunctionComponent } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 type LogInHeaderProps = {};
 
 const LogInHeader: FunctionComponent<LogInHeaderProps> = () => {
+  const navigate = useNavigate();
+
+  const onClickGoBackButtonHandler = () => {
+    navigate("/");
+  };
+
   return (
     <LogInHeaderContainer>
-      <GoBackButton>{"<"}</GoBackButton>
+      <GoBackButton onClick={onClickGoBackButtonHandler}>
+        <img src="https://wliv.kr/img/arrow-left-icon.svg" alt="뒤로 가기" />
+      </GoBackButton>
       <LogInTitle>로그인</LogInTitle>
     </LogInHeaderContainer>
   );
