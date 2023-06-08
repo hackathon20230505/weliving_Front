a5eefe802e81f8be6db904ada3da4486f09bb3cf
LAPTOP-GR2A5CGU\이소윤 <99dlthdbs@gmail.com>
2023-06-08 17:50:32 +0900

    [#57] Feat: Implement Home page
    로그인 이후에 메인 페이지를 구현합니다.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,21 @@
 import { FunctionComponent } from "react";
+import styled from "styled-components";
+import HomeHeader from "../components/Home/HomeHeader";
+import HomeBody from "../components/Home/HomeBody";
 type HomeProps = {};
 
 const Home: FunctionComponent<HomeProps> = () => {
-  return <div>Home</div>;
+  return (
+    <HomeContainer>
+      <HomeHeader />
+      <HomeBody />
+    </HomeContainer>
+  );
 };
 
 export default Home;
+
+const HomeContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
