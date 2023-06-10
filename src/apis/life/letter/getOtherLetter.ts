35b2e1c6cd5e3c62f42c5eadbaf707d7ead9f141
99dlthdbs <99dlthdbs@gmail.com>
2023-06-11 06:00:21 +0900

    Feat: 다른 사람 일지 불러오기 API 수정

--- src/apis/life/letter/getOtherLetter.ts
+++ src/apis/life/letter/getOtherLetter.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const getOtherLetter = async (letter_id: string) => {
-  // send letter_id to server
+const getOtherLetter = async (letter_id?: string) => {
+  if (!letter_id) {
+    return undefined;
+  }
+
   const { data } = await axios.get(`/api/life/letter/othershow`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
